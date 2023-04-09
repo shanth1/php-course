50e487b98dbf243862d971beba62f6702de248ef
Denis Beresnev <shanthi.bunch@gmail.com>
2023-04-09 09:49:30 +0300

    if-statement

--- index.php
+++ index.php
@@ -1,28 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <form action="index.php" method="post">
-        <label>name: </label>
-        <input type="text" name="username"><br>
-        <label>password: </label>
-        <input type="password" name="password"><br>
-        <input type="submit" value="Log in">
-    </form>
-</body>
-
-</html>
-
-
 <?php
-echo $_POST;
-echo "<br>";
-echo $_POST["username"] . "<br>";
-echo "{$_POST["password"]} <br>";
+$age = 19;
+
+
+if ($age == 100) {
+    echo "Wow";
+} else if ($age < 0) {
+    echo "impossible";
+} else if ($age >= 18) {
+    echo "Yes";
+} else {
+    echo "Ho-ho-ho, No";
+}
