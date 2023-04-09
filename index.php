98f40abad78d03847616eaccf379fe6eb0b9f277
Denis Beresnev <shanthi.bunch@gmail.com>
2023-04-09 14:10:39 +0300

    logical operation

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 $age = 19;
+$name = "Denis";
+$isLogged = true;
+$isBaned = false;
 
-
-if ($age == 100) {
+if ($age == 100 || $name == "Bob") {
     echo "Wow";
 } else if ($age < 0) {
     echo "impossible";
-} else if ($age >= 18) {
+} else if ($age >= 18 && $isLogged && !$isBaned) {
     echo "Yes";
 } else {
     echo "Ho-ho-ho, No";
