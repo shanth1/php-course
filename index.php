7fa948360be7a2d3fc06fe1a299dc7313eff03a3
Denis Beresnev <shanthi.bunch@gmail.com>
2023-04-10 14:03:40 +0300

    arrays and main array function

--- index.php
+++ index.php
@@ -10,25 +10,56 @@
 
 <body>
     <form action="index.php" method="get">
-        <label>Enter start value</label>
-        <input type="text" value="1" name="start">
+        <label>Enter a word</label>
+        <input type="text" name="word">
+        <input type="submit" value="push" name="button">
         <br>
-        <label>Enter end value</label>
-        <input type="text" value="10" name="end">
-        <br>
-        <input type="submit" value="Go!">
+        <input type="submit" value="pop" name="button">
+        <input type="submit" value="shift" name="button">
+        <input type="submit" value="reverse" name="button">
+        <input type="submit" value="count" name="button">
     </form>
 </body>
 
 </html>
 
 <?php
-$start = $_GET['start'];
-$end = $_GET['end'];
+$button_type = $_GET['button'];
+$words = array('greet', 'liver', 'dress', 'suit');
+echo  "words array before:" . "<br>";
+foreach ($words as $word) {
+    echo $word . "<br>";
+}
+echo "<br>";
 
-$number = $start;
-while ($number <= $end) {
-    $square = $number ** 2;
-    echo "$number ** 2 = {$square}" . "<br>";
-    $number++;
+switch ($button_type) {
+    case 'push':
+        echo "pressed the push button" . "<br>";
+        array_push($words, $_GET['word']);
+        break;
+    case 'pop':
+        echo "pressed the pop button" . "<br>";
+        array_pop($words);
+        break;
+    case 'shift':
+        echo "pressed the shift button" . "<br>";
+        array_shift($words);
+        break;
+    case 'reverse':
+        echo "pressed the reverse button" . "<br>";
+        $words = array_reverse($words);
+        break;
+    case 'count':
+        echo "pressed the count button" . "<br>";
+        echo count($words) . "<br>";
+        break;
+    default:
+        echo "unknown button type" . "<br>";
+        break;
+}
+
+echo '<br>';
+echo  "words array after:" . "<br>";
+foreach ($words as $word) {
+    echo $word . "<br>";
 }
