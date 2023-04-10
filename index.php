1684f5ab0cc36463c6ec3e6b0b8f559288455a59
Denis Beresnev <shanthi.bunch@gmail.com>
2023-04-10 12:10:33 +0300

    for loop with square number and get method

--- index.php
+++ index.php
@@ -1,19 +1,32 @@
-<?php
-$grade = "A";
+<!DOCTYPE html>
+<html lang="en">
 
-switch ($grade) {
-    case 'A':
-        echo "Great!";
-        break;
-    case 'B':
-    case 'C':
-    case 'D':
-        echo "Ok!";
-        break;
-    case 'F':
-        echo "Bad!";
-        break;
-    default:
-        echo "{$grade} is not a grade!";
-        break;
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="index.php" method="get">
+        <label>Enter start value</label>
+        <input type="text" value="1" name="start">
+        <br>
+        <label>Enter end value</label>
+        <input type="text" value="10" name="end">
+        <br>
+        <input type="submit" value="Go!">
+    </form>
+</body>
+
+</html>
+
+<?php
+$start = $_GET['start'];
+$end = $_GET['end'];
+
+for ($number = $start; $number <= $end; $number++) {
+    $square = $number ** 2;
+    echo "$number ** 2 = {$square}" . "<br>";
 }
