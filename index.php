c54d8c9030599b473b7e146a600fe34a42607413
Denis Beresnev <shanthi.bunch@gmail.com>
2023-04-10 16:35:35 +0300

    isset and empty function
    echo in ternary operator

--- index.php
+++ index.php
@@ -1,33 +1,11 @@
-
 <?php
-$dictionary = array(
-    'academy' => 'академия',
-    'board' => 'доска',
-    'suspect' => 'подозревать',
-);
-echo "original associative array:" . "<br>";
-foreach ($dictionary as $key => $value) {
-    echo "$key = $value" . "<br>";
-}
+/*
+    ? isset() - returns TRUE if a variable is declared and not null
+    ? empty() - returns TRUE if a variable is not declared, false, null, ""
+*/
 
-$keys = array_keys($dictionary);
-$values = array_values($dictionary);
-$flipped_array = array_flip($dictionary);
+$username = null;
 
-echo "<br><br>";
-echo "only keys:" . "<br>";
-foreach ($keys as $key) {
-    echo $key . "<br>";
-}
-
-echo "<br><br>";
-echo "only values:" . "<br>";
-foreach ($values as $value) {
-    echo $value . "<br>";
-}
-
-echo "<br><br>";
-echo "flipped array:" . "<br>";
-foreach ($flipped_array as $key => $value) {
-    echo "$key = $value" . "<br>";
-}
+echo isset($username) ? "isset returns true" : "isset returns false";
+echo "<br>";
+echo empty($username) ? "empty returns true" : "empty returns false";
