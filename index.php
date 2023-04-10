2b6048a333cbed2d6d4a8d89b52761b4ca6ea2fe
Denis Beresnev <shanthi.bunch@gmail.com>
2023-04-11 00:43:56 +0300

    string functions
    strtolower
    strtoupper
    trim
    str_pad
    str_replace
    strrev
    str_shuffle
    strcmp
    strlen
    strpos
    substr
    explode
    implode

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
 <?php
-function greeting(string $name)
-{
-    echo "Hello, $name";
-}
+$arr = array("Hello", ",", "world", "!");
+$text = "___Q W E r_t_y#";
 
-function sum(int $a, int $b)
-{
-    return $a + $b;
-}
-
-greeting("Denis");
+echo strtolower($text) . "<br>";
+echo strtoupper($text) . "<br>";
+echo "trim: " . trim($text, "_") . "<br>";
+echo "pad: " . str_pad($text, 21, "@", STR_PAD_BOTH) . "<br>";
+echo "replace: " . str_replace("_", " ", $text) . "<br>";
+echo "reverse: " . strrev($text) . "<br>";
+echo "shuffle: " . str_shuffle($text) . "<br>";
+echo strcmp($text, "___Q W E r_t_sy#") ? "different string" : "same string";
 echo "<br>";
-echo sum(10, 20);
+echo "length: " . strlen($text) . "<br>";
+echo "position of E: " . strpos($text, "E") . "<br>";
+echo "substring: " . substr($text, 0, 7) . "<br>";
+
+echo "<br>";
+echo "string to array: <br>";
+foreach (explode(' ', $text) as $word) {
+    echo $word . "<br>";
+}
+echo "<br>";
+
+echo "array to string: " . implode(" ", $arr) . "<br>";
