1a73efa342898f1a87be7dbb99e8a7bc587491b7
Denis Beresnev <shanthi.bunch@gmail.com>
2023-04-12 23:10:49 +0300

    include function, tel and mailto attributes
    include function copies the content of a file (php/html/text)
    and includes it in your php file.
    Section of your website become reusable
    Changes only need to be made in one place

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+include("header.html");
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,38 +12,10 @@
 </head>
 
 <body>
-    <form action="index.php" method="post">
-        <label>Username:</label>
-        <input type="text" name="username"><br>
-        <label>Age:</label>
-        <input type="text" name="age"><br>
-        <label>Email:</label>
-        <input type="text" name="email"><br>
-        <input type="submit" name="login" value="Log in">
-    </form>
+    <h3>This is a home page</h3>
 </body>
 
 </html>
 <?php
-if (isset($_POST['login'])) {
-    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
-    $age = filter_input(INPUT_POST, "age", FILTER_SANITIZE_NUMBER_INT);
-    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
-
-    $age = filter_input(INPUT_POST, "age", FILTER_VALIDATE_INT);
-    $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
-
-    echo "Hi, $username" . "<br>";
-
-    if (empty($age)) {
-        echo "Age is not valid<br>";
-    } else {
-        echo "You are $age years old" . "<br>";
-    }
-
-    if (empty($email)) {
-        echo "Email is not valid<br>";
-    } else {
-        echo "Email: $email" . "<br>";
-    }
-}
+include("footer.html");
+?>
