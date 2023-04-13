6403f0fd54e47654786cfd67592c018c9e2787fb
Denis Beresnev <shanthi.bunch@gmail.com>
2023-04-13 13:27:17 +0300

    _SERVER and some new features
    SERVER is SGB (super global variable) that contains headers, paths
    and script locations.
    The entries in this array are created by web server.
    Shows nearly everything you need to know
    about the current web page environment
    
    Also used:
    htmlspecialchars function and PHP_SELF key in html action form
    new way of handling post request through REQUEST_METHOD key

--- index.php
+++ index.php
@@ -1,37 +1,20 @@
-<?php
-session_start();
-echo "username from session: {$_SESSION["username"]}" . "<br>";
-echo "password from session: {$_SESSION["password"]}" . "<br>";
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <form action="index.php" method="post">
-        <label>username: </label>
-        <input type="text" name="username">
-        <label>password: </label>
-        <input type="password" name="password">
-        <hr>
-        <input type="submit" name="login" value="Log in">
-    </form>
-</body>
+<html>
+<h1>All SERVER values</h1>
+<form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+    <input type="submit">
+</form>
+<hr>
 
 </html>
 <?php
-if (isset($_POST["login"])) {
-    if (!empty($_POST["username"]) && !empty($_POST["password"])) {
-        $_SESSION["username"] = $_POST["username"];
-        $_SESSION["password"] = $_POST["password"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    echo "REQUEST_METHOD is POST";
+} else {
+    echo "REQUEST_METHOD is not a POST";
+}
 
-        echo "ok";
-        header("loCation: home.php");
-    }
+echo "<hr>";
+
+foreach ($_SERVER as $key => $value) {
+    echo "$key: $value <br>";
 }
