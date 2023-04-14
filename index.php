157bb2b6a3212f133c4a865ba1c6e19d88237090
Denis Beresnev <shanthi.bunch@gmail.com>
2023-04-14 19:29:48 +0300

    getted data with while and mysqli_fetch_assoc
    mysqli_num_rows function
    mysqli_fetch_assoc function
    while statement for getting multiple rows

--- index.php
+++ index.php
@@ -1,32 +1,17 @@
 <?php
 include("database.php");
-$username = "Oleg";
-$password = "blem";
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-$age = 30;
-$sql_query = "INSERT INTO users (username, password, age) 
-                VALUES ('$username', '$hashed_password', $age)";
+$sql_query = "SELECT username, age FROM users WHERE age > 20";
+
+
 try {
-    mysqli_query($connection, $sql_query);
-    echo "User is added";
+    $result = mysqli_query($connection, $sql_query);
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo $row["username"] . " " . $row["age"] . "<br>";
+        }
+    }
 } catch (mysqli_sql_exception) {
-    echo "Could not added a user";
+    echo "No results found ";
 }
 
-mysqli_close($connection)
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    Hello
-</body>
-
-</html>
+mysqli_close($connection);
